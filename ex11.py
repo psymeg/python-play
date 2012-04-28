3944b242f97ff6e4464e7eb5bab15d32289a34ee
Simon Gibson <terryteo@gmail.com>
2012-04-28 12:18:27 +1000

    Add .gitignore and some more exercises

---
+++ ex11.py
@@ -0,0 +1,12 @@
+#lpthw ex11.py raw_input
+
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, you are %r years old, %r tall and %r heavy." % (age, height, weight)
+print "So, you are %s years old, %s tall and %s heavy." % (age, height, weight)
+
---
+++ ex12.py
@@ -0,0 +1,8 @@
+#lpthw ex12.py prompts and input
+
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+weight = raw_input("How much do you weigh? ")
+
+print "So, you are %r old, %r tall and weigh %r." % (age, height, weight)
+print "So, you are %s old, %s tall and weigh %s." % (age, height, weight)
